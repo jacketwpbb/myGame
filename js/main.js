324610273f1026f23e6482ce99f5e6704336ee1d
Grenade <532291545@qq.com>
2017-08-25 11:11:26 +0800

    抽象场景1

--- js/main.js
+++ js/main.js
@@ -9,10 +9,10 @@ var loadLevel = function (game,n) {
     }
     return blocks
 }
-var blocks
+
 var pause=false
 var fps=5
-var score=0
+// var score=0
 
 var enableDebugMode=function(game,enable){
     if(!enable){
@@ -44,86 +44,19 @@ var __main = function () {
     }
     var game = Game(images,function(game){
 
-        var paddle = Paddle(game)
-
-        var ball = Ball(game)
-        blocks = loadLevel(game,1)
-
-        game.registerAction("a", function () {
-            paddle.moveLeft()
-        })
-        game.registerAction("d", function () {
-            paddle.moveRight()
-        })
-        game.registerAction("f", function () {
-            ball.fired = true
-        })
-
-
+        var scene=Scene(game)
         game.update = function () {
             if (pause) {
                 return
             }
-            ball.move()
-            //collide
-            ball.bounce(ball.collide(paddle), paddle)
-            for (var i = 0; i < blocks.length; i++) {
-                var block = blocks[i];
-                ball.bounce(block.alive && ball.collide(block),block)
-                if(block.alive && ball.collide(block)){
-                    block.kill()
-                    score+=100
-                }
-
-            }
-
+            scene.update()
 
         }
 
-        //mouse event鼠标拖拽
-        var enableDrag=false
-        game.canvas.addEventListener('mousedown',(event)=>{
-            var x=event.offsetX
-            var y=event.offsetY
-            //检查是否点中了ball
-            if(ball.hasPoint(x,y)){
-                //设置拖拽状态
-                enableDrag=true
-            }
-        })
-        game.canvas.addEventListener('mousemove',(event)=>{
-            var x=event.offsetX
-            var y=event.offsetY
-            if(enableDrag){
-                ball.x=x
-                ball.y=y
-            }
-
-        })
-        game.canvas.addEventListener('mouseup',(event)=>{
-            var x=event.offsetX
-            var y=event.offsetY
-            enableDrag=false
-        })
 
         //开始画
         game.draw = function () {
-            //background
-            game.context.fillStyle="#ccc"
-            game.context.fillRect(0,0,400,300)
-
-            //draw
-            game.drawImage(ball)
-            game.drawImage(paddle)
-            for (var i = 0; i < blocks.length; i++) {
-                var b = blocks[i];
-                if (b.alive) {
-                    game.drawImage(b)
-                }
-
-            }
-            //label
-            game.context.fillText('分数'+score,10,290)
+            scene.draw()
 
         }
     })
---
+++ js/scene.js
@@ -0,0 +1,83 @@
+var Scene=function(game){
+    var s={
+        game:game,
+
+    }
+
+    //初始化
+    var score=0
+    var paddle = Paddle(game)
+    var ball = Ball(game)
+    var blocks = loadLevel(game,1)
+
+    //注册按键
+    game.registerAction("a", function () {
+        paddle.moveLeft()
+    })
+    game.registerAction("d", function () {
+        paddle.moveRight()
+    })
+    game.registerAction("f", function () {
+        ball.fired = true
+    })
+
+    s.update=function(){
+        ball.move()
+        //collide
+        ball.bounce(ball.collide(paddle), paddle)
+        for (var i = 0; i < blocks.length; i++) {
+            var block = blocks[i];
+            ball.bounce(block.alive && ball.collide(block),block)
+            if(block.alive && ball.collide(block)){
+                block.kill()
+                score+=100
+            }
+        }
+        //mouse event鼠标拖拽
+        var enableDrag=false
+        game.canvas.addEventListener('mousedown',(event)=>{
+            var x=event.offsetX
+            var y=event.offsetY
+            //检查是否点中了ball
+            if(ball.hasPoint(x,y)){
+                //设置拖拽状态
+                enableDrag=true
+            }
+        })
+        game.canvas.addEventListener('mousemove',(event)=>{
+            var x=event.offsetX
+            var y=event.offsetY
+            if(enableDrag){
+                ball.x=x
+                ball.y=y
+            }
+
+        })
+        game.canvas.addEventListener('mouseup',(event)=>{
+            var x=event.offsetX
+            var y=event.offsetY
+            enableDrag=false
+        })
+
+    }
+    s.draw=function(){
+        //background
+        game.context.fillStyle="#ccc"
+        game.context.fillRect(0,0,400,300)
+
+        //draw
+        game.drawImage(ball)
+        game.drawImage(paddle)
+        for (var i = 0; i < blocks.length; i++) {
+            var b = blocks[i];
+            if (b.alive) {
+                game.drawImage(b)
+            }
+
+        }
+        //label
+        game.context.fillText('分数'+score,10,290)
+    }
+
+    return s
+}
